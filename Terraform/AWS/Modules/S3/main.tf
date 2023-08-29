ac792d1c4563c6c16d930d9f24ec6216e3d9ae0b
PabloRodriguezDBP <pablo.rodriguez@dualbootpartners.com>
2023-08-28 22:30:14 -0300

    Add S3 module

---
+++ Terraform/AWS/Modules/S3/main.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "bucket" { 
+  bucket = var.bucket_name 
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.env_name
+  }
+
+}
+/*
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    =  var.acl
+}*/
+
+resource "aws_s3_bucket_cors_configuration" "bucket_cors" {
+  count = length(var.cors_rules_list) == 0 ? 0 : 1
+
+  bucket = aws_s3_bucket.bucket.id
+  
+  dynamic "cors_rule" {
+    for_each = var.cors_rules_list
+    
+    content {
+      allowed_headers = cors_rule.value.allowed_headers
+      allowed_methods = cors_rule.value.allowed_methods
+      allowed_origins = cors_rule.value.allowed_origins
+      expose_headers  = cors_rule.value.expose_headers
+      max_age_seconds = cors_rule.value.max_age_seconds
+    }
+  }  
+}
+
+//"[${cors_rule.value.allowed_headers}]"
